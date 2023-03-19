ad146a609c6b3f45ba0c9dfb3b3d23fc5d9b672d
TechSamar <TechSamar@192.168.1.42>
2023-03-20 00:05:28 +0530

    add two numbers

---
+++ AddTwoNumber/src/com/ts/add/Add.java
@@ -0,0 +1,13 @@
+package com.ts.add;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a=65;
+		int b=15;
+		int c=a+b;
+		System.out.println("a = "+a+", b = "+b);
+		System.out.println("a+b = "+c);
+	}
+
+}
